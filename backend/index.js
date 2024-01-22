c1e4282c6c55e89b7c928e5a78dc14a36c63751f
Abhishek Sharma <sharma312006@gmail.com>
2024-01-22 11:16:15 +0530

    database connection

--- backend/index.js
+++ backend/index.js
@@ -1,4 +1,15 @@
 const express = require("express");
 require("dotenv").config();
-
-console.log(process.env.MONGODB_URI);
+const mongoose = require("mongoose");
+const app = express();
+mongoose
+  .connect(`${process.env.MONGODB_URI}/paytm`)
+  .then((value) => {
+    console.log(`DB connected`);
+    app.listen(8000, () => {
+      console.log(`App is listening on port https://localhost:8000`);
+    });
+  })
+  .catch((error) => {
+    console.error(`mongoose db connection failed: ${error.message}`);
+  });
